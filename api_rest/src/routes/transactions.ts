b08da2e44ff2a68e71436bca1aeaf3c36a173d0d
thales0591 <thales0591@gmail.com>
2025-03-01 20:55:41 -0300

    feat: create post transactions route and validate it with zod

--- api_rest/src/routes/transactions.ts
+++ api_rest/src/routes/transactions.ts
@@ -1,16 +1,24 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
+import { randomUUID } from 'node:crypto'
 
-export function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const test = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'titulo teste',
-        amount: 1000,
-      })
-      .returning('*')
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createRequestBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return test
+    const { title, amount, type } = createRequestBodySchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
