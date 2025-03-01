54e25c50a461c4b766cbbdc273ab734799f4df91
thales0591 <thales0591@gmail.com>
2025-03-01 14:37:10 -0300

    create transactions table migrations and test it

--- api_rest/src/server.ts
+++ api_rest/src/server.ts
@@ -1,10 +1,17 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = await knex('sqlite_schema').select('*')
+  const test = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'titulo teste',
+      amount: 1000,
+    })
+    .returning('*')
 
   return test
 })
