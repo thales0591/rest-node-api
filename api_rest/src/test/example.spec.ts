3a7641be951c0cef08874b191d5009054b2752d9
thales0591 <thales0591@gmail.com>
2025-03-02 15:11:01 -0300

    feat: create post route test with vitest and supertest

---
+++ api_rest/src/test/example.spec.ts
@@ -0,0 +1,22 @@
+import { beforeAll, test, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../app'
+
+beforeAll(async () => {
+  await app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'new transaction',
+      amount: 200,
+      type: 'credit',
+    })
+    .expect(201)
+})
