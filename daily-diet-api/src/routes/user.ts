fbb96c91b5f37163097427bfc2311eba9448dac6
thales0591 <thales0591@gmail.com>
2025-03-04 16:09:04 -0300

    feat: create /login route

--- daily-diet-api/src/routes/user.ts
+++ daily-diet-api/src/routes/user.ts
@@ -4,19 +4,43 @@ import { z } from 'zod'
 import { randomUUID } from 'crypto'
 
 export async function createUserRoutes(app: FastifyInstance) {
-  app.post('/register', async (request, reply) => {
-    const createRegisterSchema = z.object({
+  app.post('/login', async (request, reply) => {
+    const getLoginSchema = z.object({
       username: z.string(),
       password: z.string(),
     })
 
-    const { username, password } = createRegisterSchema.parse(request.body)
+    const { username, password } = getLoginSchema.parse(request.body)
 
-    const userAlredyCreated = await knex('users')
+    const searchUserResponse = await knex('users')
+      .where({
+        name: username,
+        password,
+      })
+      .first()
+
+    if (!searchUserResponse) {
+      return reply.status(404).send({ message: 'User not found.' })
+    }
+
+    reply.setCookie('sessionId', searchUserResponse.id)
+
+    reply.status(200).send({ message: 'Successfully logged in.' })
+  })
+
+  app.post('/register', async (request, reply) => {
+    const getRegisterSchema = z.object({
+      username: z.string(),
+      password: z.string(),
+    })
+
+    const { username, password } = getRegisterSchema.parse(request.body)
+
+    const userAlredyCreatedResponse = await knex('users')
       .where('name', username)
       .first()
 
-    if (userAlredyCreated) {
+    if (userAlredyCreatedResponse) {
       return reply.status(409).send({
         message:
           'A user with this username already exists, please choose another one.',
