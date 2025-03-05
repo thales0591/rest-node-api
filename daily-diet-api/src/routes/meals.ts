b50fab3ccc93542dea61800344977bbfec14d87e
thales0591 <thales0591@gmail.com>
2025-03-05 12:32:52 -0300

    feat: implement the get, put and delete routes that use id params

--- daily-diet-api/src/routes/meals.ts
+++ daily-diet-api/src/routes/meals.ts
@@ -6,7 +6,7 @@ import { checkUserIsLogged } from '../middlewares/check-user-is-logged'
 
 export async function createMealsRoutes(app: FastifyInstance) {
   app.get(
-    '/meals',
+    '/',
     {
       preHandler: [checkUserIsLogged],
     },
@@ -21,8 +21,38 @@ export async function createMealsRoutes(app: FastifyInstance) {
     },
   )
 
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkUserIsLogged],
+    },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const sessionId = request.cookies.sessionId
+
+      const { id } = getMealParamsSchema.parse(request.params)
+
+      const selectedMealResponse = await knex('meals').where('id', id).first()
+
+      if (selectedMealResponse?.userId !== sessionId) {
+        return reply
+          .status(401)
+          .send({ message: 'You do not have access to this meal.' })
+      }
+
+      if (!selectedMealResponse) {
+        return reply.status(404).send({ message: 'Meal not found.' })
+      }
+
+      return reply.status(200).send(selectedMealResponse)
+    },
+  )
+
   app.post(
-    '/meals',
+    '/',
     {
       preHandler: [checkUserIsLogged],
     },
@@ -46,7 +76,86 @@ export async function createMealsRoutes(app: FastifyInstance) {
         userId: sessionId,
       })
 
-      reply.status(201).send({ message: 'Meal successfully added' })
+      return reply.status(201).send({ message: 'Meal successfully added' })
+    },
+  )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkUserIsLogged],
+    },
+    async (request, reply) => {
+      const getMealAttributesSchema = z.object({
+        name: z.string().optional(),
+        description: z.string().optional(),
+        createdAt: z.string().datetime().optional(),
+        inDiet: z.boolean().optional(),
+      })
+
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getMealParamsSchema.parse(request.params)
+      const sessionId = request.cookies.sessionId
+
+      const { name, description, createdAt, inDiet } =
+        getMealAttributesSchema.parse(request.body)
+
+      const selectedMealResponse = await knex('meals').where('id', id).first()
+
+      if (selectedMealResponse?.userId !== sessionId) {
+        return reply
+          .status(401)
+          .send({ message: 'You do not have access to this meal.' })
+      }
+
+      const updateMealResponse = await knex('meals')
+        .update({
+          name,
+          description,
+          created_at: createdAt,
+          inDiet,
+        })
+        .where('id', id)
+
+      if (!updateMealResponse) {
+        return reply.status(404).send({ message: 'Meal not found.' })
+      }
+
+      return reply.status(201).send({ message: 'Meal successfully updated' })
+    },
+  )
+
+  app.delete(
+    '/:id',
+    {
+      preHandler: [checkUserIsLogged],
+    },
+    async (request, reply) => {
+      const getMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const { id } = getMealParamsSchema.parse(request.params)
+      const sessionId = request.cookies.sessionId
+
+      const selectedMealResponse = await knex('meals').where('id', id).first()
+
+      if (selectedMealResponse?.userId !== sessionId) {
+        return reply
+          .status(401)
+          .send({ message: 'You do not have access to this meal.' })
+      }
+
+      const deletedMealResponse = await knex('meals').del().where('id', id)
+
+      if (!deletedMealResponse) {
+        return reply.status(404).send({ message: 'Meal not found.' })
+      }
+
+      return reply.status(200).send({ message: 'Meal successfully deleted' })
     },
   )
 }
